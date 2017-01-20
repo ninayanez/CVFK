0421efb8cd7067a29cb5b9b09aa9ffb318a518f8
ninayanez <nina@theblacksea.cc>
2017-01-19 16:09:44 -0800

    simplify app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 import api from './api.js'
 import ctx from './canvas.js'
 import prompt from './prompt.js'
-let conf = false
-let m = {x:100,y:60}
 
-prompt.s.pipe(ctx).pipe(api)
-
-window.addEventListener('keyup', (e) => {
-  if (e.keyCode===78&&e.ctrlKey) prompt.visible(m)
-}, false)
-
-window.addEventListener('mousemove', (e) => { m = {x:e.pageX,y:e.pageY} })
+prompt.pipe(ctx).pipe(api)
